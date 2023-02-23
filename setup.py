4aa5dbe9cabc72573b3d061b5faf1c3d8a18b21e
Carl Kadie <carlk@msn.com>
2023-02-23 09:34:24 -0800

    ~=3.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.1.0",
     description="A Python package for painting mattes",
     install_requires=["pysnptools", "pillow"],
-    python_requires="==3.8",
+    python_requires="~=3.8",
     license="proprietary",
     packages=["anipaint", "anipaint/tests"],  # basically, everything with a __init__.py
 )
